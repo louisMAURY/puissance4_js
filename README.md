1a841418b884ec9ef5653f2777d474048b62efdf
louisMAURY <louismaury33@gmail.com>
2019-04-26 11:24:30 +0200

    J'ai du mal...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Puissance4_js
 Le jeu du puissance 4 fait en javascript
+
+Enfin c'est le but
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 'use strict'
 
-function debut() {
-    console.log("Ça va bientôt commencer")
-    
-};
+Boolean kase = false // Pour savoir si la case est occupé (true) ou non (false)
+let jaune = "url(jetons/jaune.png)" // variable pour l'image du jeton jaune
+let rouge = "url(jetons/rouge.png)" // variable pour l'image du jeton rouge
+
+var premireLigne = document.getElementById("A"); // Selectionne la première ligne du plateau
+
+// Au survole de la ligne
+premireLigne.addEventListener("mouseover", function(event) {   
+    event.target.style.backgroundImage = "url(jetons/jaune.png)";
+
+    setTimeout(function() {
+      event.target.style.backgroundImage = "url(jetons/vide.png)";
+    }, 500);
+  }, false);
+
+// Cette partie s'occupe du tour du joueur
+premireLigne.addEventListener("click", event => {
+  if (kase == false) {
+    // remplie la case avec un jeton
+  } else {
+    // Passe a la case au dessus
+  }
+})
